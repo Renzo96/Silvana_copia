8d5a1cf5af368a57250663c14ecb9f44a404b4ce
Renzo Sosa <67750743+Renzo96@users.noreply.github.com>
2023-05-28 20:17:22 -0300

    fiveth

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ from starlette import responses
 app = FastAPI()
 security = HTTPBasic()
 
-db_host = os.environ.get("DB_HOST")
-db_port = int(os.environ.get("DB_PORT"))
-db_user = os.environ.get("DB_USER")
-db_password = os.environ.get("DB_PASSWORD")
-db_name = os.environ.get("DB_NAME")
+db_host = os.getenv("DB_HOST")
+db_port = int(os.getenv("DB_PORT"))
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_name = os.getenv("DB_NAME")
 
 
 @app.get("/")
