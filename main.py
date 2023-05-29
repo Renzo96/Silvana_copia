af8d99a792b58433d9b9bda8e631306bf56e1a4e
Renzo Sosa <67750743+Renzo96@users.noreply.github.com>
2023-05-29 15:57:59 -0300

    EIGHT

--- main.py
+++ main.py
@@ -162,4 +162,4 @@ async def logout(credentials: HTTPBasicCredentials = Depends(security)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0"# , port=8000)
+    uvicorn.run(app, host="0.0.0.0" , port=3306)
